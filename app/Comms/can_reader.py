2619a48b8c73b3ad6bd734b33f3df3006667f482
vijay <vijaysangoi29@gmail.com>
2024-01-11 16:58:14 +0530

    dropped files

--- app/Comms/can_reader.py
+++
@@ -1,6 +0,0 @@
-import can
-
-print_listener = can.Printer()
-bus = can.Bus(interface='socketcan',channel='vcan0',receive_own_message=True)
-print_listener = can.Printer()
-can.Notifier(bus,[print_Listener])
