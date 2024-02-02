92302413fe23f27f7b3ca678f1c8b55db07f1481
vijay <vijaysangoi29@gmail.com>
2024-02-02 10:44:06 +0530

    delete trigger

--- app/Http/Controllers/Web/TriggersController.php
+++ app/Http/Controllers/Web/TriggersController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Models\Triggers;
+use App\Models\Worker;
 
 class TriggersController extends Controller
 {
@@ -14,29 +17,50 @@ class TriggersController extends Controller
     public static function list_trigger(Request $req)
     {
         if ($req->isMethod('post')) {
+            if(empty($_POST['name'])) goto chk;
             $name = $_POST['name'];
-            if(!isset($_POST['id']))
-            {
+            if (!isset($_POST['id'])) {
                 $query = Triggers::create(
-                    ["name" => $name],
+                    [
+                        "name" => $name,
+                        "endpoint" => Str::uuid(),
+                    ],
                 );
             }
-            if(isset($_POST['id']))
-            {
+            if (isset($_POST['id'])) {
                 $id = $_POST['id'];
-                $rec = Triggers::where("id",$id)->first();
+                $rec = Triggers::where("id", $id)->first();
                 $rec->name = $name;
                 $rec->save();
             }
         }
+        if ($req->isMethod('delete')) {
+            $id = $_POST['id'];
+            $rec = Triggers::where("id", $id)->first();
+            $rec->delete();
+        }
+        chk:
         $size = $_GET['size'] ?? $_POST['size'] ?? 10;
         $page = $_GET['page'] ?? $_POST['page'] ?? 1;
-        $query = Triggers::select('*');
-        $rec = $query->paginate($size,['*'],'page',$page);
+        $query = Triggers::select('id', 'name', 'jobs', db::raw("concat('api/trigger/',endpoint) as url"), 'created_at', 'updated_at');
+        $rec = $query->paginate($size, ['*'], 'page', $page);
         return $rec;
     }
     public function index()
     {
-        return view('web.triggers');
+        $qy = Worker::select('name', 'id');
+        $work = $qy->get();
+        $work = $work->map(function ($ci) {
+            return [$ci->name, $ci->id];
+        });
+        $files = scandir(app_path('Jobs'));
+        unset($files[0]);
+        unset($files[1]);
+        $files = collect(array_values($files));
+        $files = $files->map(function ($ci) {
+            $nmx = str_replace(".php", "", $ci);
+            return [$nmx, $nmx];
+        });
+        return view('web.triggers')->with('wo', $work)->with('ff', collect($files));
     }
 }
