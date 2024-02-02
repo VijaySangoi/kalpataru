f4eb2216e78b1ffa9667f45244dbe19393f54c2a
vijay <vijaysangoi29@gmail.com>
2024-02-02 10:57:34 +0530

    sensor model

---
+++ app/Models/Sensor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'value',
+        'node',
+        'ui_x',
+        'ui_y',
+    ];
+    protected $casts = [
+        'created_at' => 'date:d-m-Y',
+        'updated_at' => 'date:d-m-Y'
+    ];
+}
