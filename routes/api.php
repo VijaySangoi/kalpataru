dbb50f75f78ccf48897d41900f07e6cf9d3f0757
vijay <vijaysangoi29@gmail.com>
2024-02-02 10:43:09 +0530

    delete part 2

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,13 @@ Route::middleware('auth:sanctum')->group( function () {
     Route::get('/list-log-file','App\Http\Controllers\Api\AllApiController@list_log_files');
     Route::get('/trigger/{job_id}','App\Http\Controllers\Api\JobController@trigger');
     Route::get('/scrathpad','App\Http\Controllers\Web\ScrathpadController@api');
-    Route::match(['GET','POST'],'/devices/serial','App\Http\Controllers\Api\AllApiController@list_serial_devices');
-    Route::match(['GET','POST'],'/log-file','App\Http\Controllers\Api\AllApiController@log_files');
-    Route::match(['GET','POST'],'/workers','App\Http\Controllers\Api\AllApiController@list_workers');
-    Route::match(['GET','POST'],'/trigger','App\Http\Controllers\Api\AllApiController@list_trigger');
+    Route::match(['GET','POST','DELETE'],'/devices/serial','App\Http\Controllers\Api\AllApiController@list_serial_devices');
+    Route::match(['GET','POST','DELETE'],'/log-file','App\Http\Controllers\Api\AllApiController@log_files');
+    Route::match(['GET','POST','DELETE'],'/workers','App\Http\Controllers\Api\AllApiController@list_workers');
+    Route::match(['GET','POST','DELETE'],'/trigger','App\Http\Controllers\Api\AllApiController@list_trigger');
     Route::post('/cip/register','App\Http\Controllers\Api\AllApiController@cip_register');
     Route::get('/cip','App\Http\Controllers\Api\AllApiController@list_cip');
+    Route::match(['GET','POST','DELETE'],'/cip/message','App\Http\Controllers\Api\AllApiController@cip_message');
     Route::match(['GET','POST','DELETE'],'/sensors','App\Http\Controllers\Api\AllApiController@sensors');
     Route::post('/sensor_pos','App\Http\Controllers\Api\AllApiController@sensors_pos');
 });
