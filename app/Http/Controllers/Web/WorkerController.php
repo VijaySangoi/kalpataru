a433e6d1bb8a3251be459891e575227d5db666ee
vijay <vijaysangoi29@gmail.com>
2024-02-02 10:49:10 +0530

    delete worker

--- app/Http/Controllers/Web/WorkerController.php
+++ app/Http/Controllers/Web/WorkerController.php
@@ -18,13 +18,21 @@ class WorkerController extends Controller
     public static function list_workers(Request $req)
     {
         if ($req->isMethod('post')) {
+            if(empty($_POST['worker'])) goto chk;
+            $worker = $_POST['worker'];
+            if(empty($_POST['tries'])) goto chk;
+            $tries = $_POST['tries'];
+            if(empty($_POST['timeout'])) goto chk;
+            $timeout = $_POST['timeout'];
+            if(empty($_POST['memory'])) goto chk;
+            $memory = $_POST['memory'];
             if (!isset($_POST['id'])) {
                 $query = Worker::create(
                     [
-                        "name" => $_POST['worker'],
-                        "tries" => $_POST['tries'],
-                        "timeout" => $_POST['timeout'],
-                        "memory" => $_POST['memory']
+                        "name" => $worker,
+                        "tries" => $tries,
+                        "timeout" => $timeout,
+                        "memory" => $memory,
                     ],
                 );
             }
@@ -38,6 +46,12 @@ class WorkerController extends Controller
                 $rec->save();
             }
         }
+        if ($req->isMethod('delete')) {
+            $id = $_POST['id'];
+            $rec = Worker::where("id", $id)->first();
+            $rec->delete();
+        }
+        chk:
         $size = $_GET['size'] ?? $_POST['size'] ?? 10;
         $page = $_GET['page'] ?? $_POST['page'] ?? 1;
         $query = Worker::select('*');
