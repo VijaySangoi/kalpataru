36bb5f4c91edb0b5cc6fa5e56fc189e6826cd4a4
vijay <vijaysangoi29@gmail.com>
2024-02-11 23:27:42 +0530

    serial api update

--- app/Http/Controllers/Web/SerialController.php
+++ app/Http/Controllers/Web/SerialController.php
@@ -11,6 +11,69 @@ class SerialController extends Controller
     public function __construct()
     {
     }
+    /**
+     * @OA\GET(
+     *     path="/api/list_serial_devices",
+     *     summary="list of all serial device",
+     *     description="returns paginated list with size and page no",
+     *     tags={"Serial"},
+     *     security = {{ "Authorization": {} }},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="json body",
+     *        @OA\JsonContent(
+     *          required={"size","page"},
+     *          type="object",
+     *            @OA\Property(property="size", type="string", example="10", description="pagination limit"),
+     *            @OA\Property(property="page", type="string", example="1", description="page no."),
+     *        ),
+     *     ),
+     *    @OA\Response(response=200,description="OK")
+     * )
+     * 
+     * @OA\POST(
+     *     path="/api/list_serial_devices",
+     *     summary="api to update or insert",
+     *     description="performs update with id or insert with given details",
+     *     tags={"Serial"},
+     *     security = {{ "Authorization": {} }},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="json body",
+     *        @OA\JsonContent(
+     *          required={"size","page","name","port","baudrate"},
+     *          type="object",
+     *            @OA\Property(property="id", type="string", example="5", description="id of record to be updated, leave eempty if inserting new record"),
+     *            @OA\Property(property="size", type="string", example="10", description="pagination limit"),
+     *            @OA\Property(property="page", type="string", example="1", description="page no."),
+     *            @OA\Property(property="name", type="string", example="CNC-01", description="name of sensor"),
+     *            @OA\Property(property="port", type="string", example="COM8", description="name of com port"),
+     *            @OA\Property(property="baudrate", type="string", example="115200", description="baudrate of receiving microcontroller node"),
+     *        ),
+     *     ),
+     *    @OA\Response(response=200,description="OK")
+     * )
+     * 
+     * @OA\DELETE(
+     *     path="/api/list_serial_devices",
+     *     summary="delete a serial device",
+     *     description="delete a serial device from monitoring list",
+     *     tags={"Serial"},
+     *     security = {{ "Authorization": {} }},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="json body",
+     *        @OA\JsonContent(
+     *          required={"size","page","id"},
+     *          type="object",
+     *            @OA\Property(property="size", type="string", example="10", description="pagination limit"),
+     *            @OA\Property(property="page", type="string", example="1", description="page no."),
+     *            @OA\Property(property="id", type="string", example="5", description="id of record to be deleted"),
+     *        ),
+     *     ),
+     *    @OA\Response(response=200,description="OK")
+     * )
+     */
     public static function list_serial_devices(Request $req)
     {
         if ($req->isMethod('post')) {
