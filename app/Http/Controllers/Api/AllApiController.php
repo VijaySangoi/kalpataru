93fe0bd24475af5f08c5809924cb7a1192381712
vijay <vijaysangoi29@gmail.com>
2024-05-29 10:27:47 +0530

    overhaul api update

--- app/Http/Controllers/Api/AllApiController.php
+++ app/Http/Controllers/Api/AllApiController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Web\LogController;
-use App\Http\Controllers\Web\SerialController;
+use App\Http\Controllers\Web\NodesController;
+use App\Http\Controllers\Web\DevicesController;
 use App\Http\Controllers\Web\WorkerController;
 use App\Http\Controllers\Web\TriggersController;
-use App\Http\Controllers\Web\CIPController;
 use App\Http\Controllers\Web\HomeController;
-use App\Http\Controllers\Web\SensorsController;
 
 class AllApiController extends Controller
 {
@@ -27,10 +26,25 @@ class AllApiController extends Controller
         $res = LogController::log_file($req);
         return response()->json($res, 200);
     }
-    public function list_serial_devices(Request $req)
+    public function nodes(Request $req)
     {
-        $res = SerialController::list_serial_devices($req);
+        $res = NodesController::nodes($req);
         return response()->json($res, 200);
+    }
+    public function devices(Request $req)
+    {
+        $res = DevicesController::devices($req);
+        return response()->json($res, 200);
+    }
+    public function dev_pos(Request $req)
+    {
+        $res = DevicesController::dev_pos($req);
+        return response()->json($res, 200);
+    }
+    public function devices_message(Request $req)
+    {
+        $res = DevicesController::devices_message($req);
+        return $res;
     }
     public function list_workers(Request $req)
     {
@@ -41,31 +55,6 @@ class AllApiController extends Controller
     {
         $res = TriggersController::list_trigger($req);
         return response()->json($res, 200);
-    }
-    public function cip_register(Request $req)
-    {
-        $res = CIPController::cip_register($req);
-        return $res;
-    }
-    public function cip_message(Request $req)
-    {
-        $res = CIPController::cip_message($req);
-        return $res;
-    }
-    public function sensors(Request $req)
-    {
-        $res = SensorsController::sensors($req);
-        return $res;
-    }
-    public function list_cip(Request $req)
-    {
-        $res = CIPController::list_cip($req);
-        return $res;
-    }
-    public function sensors_pos(Request $req)
-    {
-        $res = SensorsController::sensors_pos($req);
-        return $res;
     }
     public function dashboard_component(Request $req)
     {
